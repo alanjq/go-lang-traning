34a988bfca8379df8018daf63169433561c75687
Alan Jimenez <alanjq@outlook.com>
2023-12-22 16:54:48 -0500

    Added Tailwind support

--- react-shop/src/App.js
+++ react-shop/src/App.js
@@ -1,3 +1,8 @@
+import { Button } from "./Button";
+
 export function App() {
-    return <h1>Hello world!</h1>;
+    return <>
+        <h1>Hello wossrld!</h1>;
+        <Button>Hello</Button>
+    </>    
 }
